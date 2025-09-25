99e0dcbd51ee28cc0d30bb224842e0b3a07d9292
shayankhannn <shayannkhannn007@gmail.com>
2025-09-25 07:25:38 -0700

    search section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,22 +2,22 @@
 <div class="space-y-10">
 
     {{-- search  --}}
-    <section>
+    <section class="text-center pt-6">
         <h1 class="font-bold text-4xl capitalize">Let's find your next job</h1>
         
-        <form action="" method="get">
-            <input type="text" placeholder="Web Developer...." class="rounded-xl bg-white/5 border border-white/10 w-full px-5 py-4">
+        <form action="" method="get" class="mt-6 ">
+            <input type="text" placeholder="Web Developer...." class="rounded-xl bg-white/5 border border-white/10 w-full max-w-xl px-5 py-4 ">
         </form>
   
     </section>
 
     {{-- job card  --}}
-    <section>
+    <section class="pt-10">
     
 <x-section-heading>Featured Jobs</x-section-heading>
 
      
-<div class="grid lg:grid-cols-3 gap-6">
+<div class="grid lg:grid-cols-3 gap-8 mt-6">
 
 <x-job-card/>
 <x-job-card/>
