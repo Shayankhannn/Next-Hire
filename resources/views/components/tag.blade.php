0c2518c529f3c13f6cdc365ee451989d724ab962
shayankhannn <shayannkhannn007@gmail.com>
2025-09-25 06:07:42 -0700

    tag custom font size issue

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,3 +1,3 @@
-            <a href="" class="bg-white/10 hover:bg-white/30 px-2 py-1 rounded-xl text-sm transition-colors duration-300">
+            <a href="" class="bg-white/10 hover:bg-white/30 px-3  py-1 rounded-xl text-xs font-bold transition-colors duration-300">
                 {{ $slot }}
             </a>
