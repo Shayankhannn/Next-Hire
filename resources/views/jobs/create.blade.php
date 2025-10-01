7adebf44c9c68f774afde242dd73c68008e71a89
shayankhannn <shayannkhannn007@gmail.com>
2025-10-01 00:09:17 -0700

    job create

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -1,11 +1,15 @@
 <x-layout>
         <x-page-heading>Create a Job</x-page-heading>
         <x-forms.divider/>
-    <div class=" max-w-2xl mx-auto">
-        <form method="POST" action="/jobs" class=" space-y-6" enctype="multipart/form-data">
-            @csrf
+    <x-forms.form method="POST" action="/jobs">
+            
             <x-forms.input name="title" type="text" label="Job Title" placeholder="e.g. Senior Laravel Developer" required/>
-            <x-forms.input name="company" type="text" label="Company Name" placeholder="e.g. Acme Corp" required/>
-            <x-forms.input name="location" type="text" label="Job Location" placeholder="e.g. Remote, Boston MA, etc." required/>
-            <x-forms.input name="email" type="email" label="Contact Email" placeholder="e.g.
-</x-layout>
+            <x-forms.input name="Salary" type="text" label="Job Salary" placeholder="e.g. $50,000" required/>
+            <x-forms.input name="location" type="text" label="Job Location" placeholder="e.g.Boston MA, etc." required/>
+            <x-forms.input name="url" type="text" label="Company Url" placeholder="e.g. https://acme.com/jobs/ceo-wanted" required/>
+            
+            <x-forms.input name="tags" type="text" label="Tags <small>Comma Separated***</small>" placeholder="e.g. frontend , video , education" required/>
+     
+            <x-forms.button>Publish</x-forms.button>
+            </x-forms.form>    
+        </x-layout>
